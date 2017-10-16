e0a61856ef0536e0308312a5daadcffb852ac84c
zakuArbor <nabimoon1234@gmail.com>
2017-10-16 13:47:16 -0400

    Add game loop and recieves user input

--- tic-tac-toe-CLI.c
+++ tic-tac-toe-CLI.c
@@ -9,16 +9,20 @@ typedef struct
 
 extern int initGame(player **players, int *moves);
 extern void drawBoard(player **players, int num_moves, int *moves);
+extern void makeMove(player **players, int *num_moves, int *moves, int *player_num);
 
 int main (int argc, char **argv) {
 	player **players;
+
+	int player_num = 0;
+	int quit = 0;
+	int num_moves = 0;
+	int moves[9];
+
 	if ((players = malloc(sizeof(player*) * 2)) == NULL) {
 		perror("malloc");
 		return(1);
 	}
-
-	int num_moves = 0;
-	int moves[9];
 
 	printf("Hello World\n");
 	
@@ -26,7 +30,13 @@ int main (int argc, char **argv) {
 		return(1);
 	}
 
-	drawBoard(players, num_moves, moves);
+	while (!quit) {
+		drawBoard(players, num_moves, moves);
+		makeMove(players, &num_moves, moves, &player_num);
+		if (num_moves == 9) {
+			printf("check win\n");
+		}
+	}
 
 	return(0);
 }
@@ -75,4 +85,14 @@ void drawBoard(player **players, int num_moves, int *moves) {
 		if (row < 2) printf("\n-----\n");
 	}
 	printf("\n");
+}
+
+void makeMove(player **players, int *num_moves, int *moves, int *player_num) {
+	int move;
+	scanf("%d", &move);
+	if (move >= 0 && move < 9 && moves[move] == -1) {
+		moves[move] = *player_num;
+		*player_num = *player_num ^ 1;
+		*num_moves += 1;
+	}
 }
