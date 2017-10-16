a880105e0391df1e1a15bdbdd11ec319a3318e37
zakuArbor <nabimoon1234@gmail.com>
2017-10-16 13:26:42 -0400

    Use array to store player structs

--- tic-tac-toe-CLI.c
+++ tic-tac-toe-CLI.c
@@ -1,47 +1,78 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct player 
+typedef struct 
 {
 	char name[10];
 	char piece; //either x or o
-};
+} player;
 
-extern int initGame(struct player *player1, struct player *player2, int *num_moves, int *moves);
+extern int initGame(player **players, int *moves);
+extern void drawBoard(player **players, int num_moves, int *moves);
 
 int main (int argc, char **argv) {
-	struct player *player1, *player2;
-	player1 = NULL;
-	player2 = NULL;
+	player **players;
+	if ((players = malloc(sizeof(player*) * 2)) == NULL) {
+		perror("malloc");
+		return(1);
+	}
+
 	int num_moves = 0;
 	int moves[9];
 
-	printf("Hello World");
-	if (initGame(player1, player2, &num_moves, moves) != 0) {
+	printf("Hello World\n");
+	
+	if (initGame(players, moves) != 0) {
 		return(1);
+	}
+
+	drawBoard(players, num_moves, moves);
+
+	return(0);
+}
+
+int initGame(player **players, int *moves) {
+	int i;
+
+	if (!(players[0] = malloc(sizeof(player)))) {
+		perror("malloc");
+		return(1);
+	}
+
+
+	if (!(players[1] = malloc(sizeof(player)))) {
+		perror("malloc");
+		return(1);
+	}
+
+	players[0]->piece = 'x';
+	players[1]->piece = 'o';
+	
+
+	for (i = 0; i < 9; i++) {
+		moves[i] = 0;
 	}
 
 	return(0);
 }
 
-
-int initGame(struct player *player1, struct player *player2, int *num_moves, int *moves) {
-	int i;
-
-	if ((player1 = malloc(sizeof(struct player))) == NULL) {
-		perror("malloc");
-		return(1);
+void drawBoard(player **players, int num_moves, int *moves) {
+	int row, col;
+	for (row = 0; row < 3; row++) {
+		for (col = 0; col < 5; col++) {
+			if (col % 2 == 0) { //a slot
+				if (num_moves > 0 && moves[(row*3) + (col/2)] != 0) { //if there is a piece placed
+					printf("%c", moves[(row*3) + (col/2)]);
+				}
+				else {
+					printf(" ");
+				}
+			} 
+			else { //a border
+					printf("|");
+			}
+		}
+		if (row < 2) printf("\n-----\n");
 	}
-
-	if ((player2 = malloc(sizeof(struct player))) == NULL) {
-		perror("malloc");
-		return(1);
-	}	
-
-	*num_moves = 0;
-
-	for (i = 0; i < 9; i++) {
-		moves[i] = 0;
-	}
-	return(0);
+	printf("\n");
 }
